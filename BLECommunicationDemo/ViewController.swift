e3c377b64e358dd8b2dcadc7a8e7c9860bec297f
Anand Upadhyay <anandgupadhyay03@gmail.com>
2024-07-19 10:36:24 +0530

    Near by Comunication using BLE

---
+++ BLECommunicationDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BLECommunicationDemo
+//
+//  Created by Anand Upadhyay on 19/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
